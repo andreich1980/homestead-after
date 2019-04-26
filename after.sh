9d836f758b862da00af6b9564f895ce76f372bea
AndrewP <spargo32@gmail.com>
2019-04-26 11:44:24 +0300

    Update after.sh
    Replace /etc/default/grub

--- after.sh
+++ after.sh
@@ -8,9 +8,15 @@ else
   git pull
 fi
 
+# Replace .bashrc
 cp /home/vagrant/.homestead-after/.bashrc /home/vagrant/.bashrc
 source /home/vagrant/.bashrc
 
+# Git config
 cp /home/vagrant/.homestead-after/.gitconfig /home/vagrant/.gitconfig
 git config --global user.name "Andrew P"
 git config --global user.email "spargo32@gmail.com"
+
+# Grub fail timeout
+cp -f /home/vagrant/.homestead-after/grub-default /etc/default/grub
+update-grub
