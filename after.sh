dd3be1855f5a6bc8e428a799eb59229241087588
AndrewP <spargo32@gmail.com>
2019-04-27 17:25:19 +0300

    Fix after.sh

--- after.sh
+++ after.sh
@@ -1,6 +1,5 @@
-sudo su -
-
 # Clone or update homestead-after
+echo 'Updating homestead-after...'
 if [ ! -d /home/vagrant/.homestead-after ]; then
   git clone git://github.com/andreich1980/homestead-after /home/vagrant/.homestead-after
 else
@@ -9,14 +8,17 @@ else
 fi
 
 # Replace .bashrc
+echo 'Updating .bashrc...'
 cp /home/vagrant/.homestead-after/.bashrc /home/vagrant/.bashrc
-source /home/vagrant/.bashrc
+. /home/vagrant/.bashrc
 
 # Git config
+echo 'Updating .gitconfig...'
 cp /home/vagrant/.homestead-after/.gitconfig /home/vagrant/.gitconfig
 git config --global user.name "Andrew P"
 git config --global user.email "spargo32@gmail.com"
 
 # Grub fail timeout
-cp -f /home/vagrant/.homestead-after/grub-default /etc/default/grub
-update-grub
+echo 'Updating grub fail timeout...'
+sudo cp -f /home/vagrant/.homestead-after/grub-default /etc/default/grub
+sudo update-grub
